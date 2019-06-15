315d80e4129b56722430c7ca3659ba7c0475b7c8
matthewnchow <40249298+matthewnchow@users.noreply.github.com>
2019-06-15 10:29:31 -0700

    Add files via upload

--- Cyc10FunctionPool.m
+++ Cyc10FunctionPool.m
@@ -15,7 +15,8 @@ function varargout = Cyc10FunctionPool(varargin)
     switch varargin{1}
         case 'ObjHand' % global C10 ?? Handle this as any other instrument,
                         % will want init and stuff later
-            [varargout{1}] = ObjHand(varargin{2});
+            [varargout{1}] = ObjHand();
+%% Primitive functions of FPGA
         case 'SetPeriod'
             [varargout{1}] = SetPeriod(varargin{2});
         case 'SetOuterPeriod'
@@ -29,13 +30,96 @@ function varargout = Cyc10FunctionPool(varargin)
             [varargout{1}] = ClearAll();
         case 'SoftTrig'
             [varargout{1}] = SoftTrig();
+        case 'SetDPeriod'
+            [varargout{1}] = SetDPeriod(varargin{2});            
 %         case 'Print' %Extensions
 %         case 'WriteSPI'
 
+%% Functions built of primitives 
+        case 'CWChannels'
+            [varargout{1}] =  CWChannels(varargin{2});
         case 'RabiConfigC10'
             [varargout{1}] = RabiConfigC10(varargin{2}, varargin{3},...
-                varargin{4}, varargin{5}, varargin{6});
+                varargin{4}, varargin{5}, varargin{6}, varargin{7}, varargin{8});
+        case 'PumpTime'
+            [varargout{1}] = PumpTime(varargin{2}, varargin{3},... % steps, dt, AOMdelay, counttime, depump time, longpump
+                varargin{4}, varargin{5}, varargin{6}, varargin{7});    
+        case 'CWESR'
+            [varargout{1}] = CWESR(varargin{2}, varargin{3}, varargin{4});
+      
+        case 'LoadSequence'
+            [varargout{1}] = LoadSequence(varargin{2});
+        otherwise
+            [varargout{1}] = 'I do not get it, Cyc10';
     end
+end
+
+function status = CWChannels(onchannels)
+
+    status = ClearAll();
+    status = status + SetPeriod(0.001);
+    status = status + SetState0(onchannels);
+
+end
+
+function [status] = LoadSequence(seq)
+    status = ClearAll();
+    status = status + SetState0(seq.state0);
+
+    edID = 0;
+    % loop through all the channels and program each edge
+    period = 0;
+    for i=1:numel(seq.ch)
+        chID = seq.ch(i).ID;
+        for j=1:length(seq.ch(i).x)
+            x = seq.ch(i).x(j) * 10^-9; %Convert to seconds
+            dx = seq.ch(i).dx(j) * 10^-9;
+            period = max(period, x + max(seq.N * dx, 0));
+            %SetEdge(edID, enable, chID, x, dx)
+            status = status + SetEdge(edID, 1, chID, x, dx);
+            edID = edID + 1;
+            if status < 0
+                return
+            end
+        end
+    end
+    cushion = 500 * 10^-9;
+    status = status + SetPeriod(period + cushion);
+    status = status + SetOuterPeriod(seq.N);
+    global gmSEQ
+    gmSEQ.period = period + cushion;
+    % Add status = status + SetDPeriod(dperiod)?
+end
+
+%% LoadYaoSequence Abandoned by MChow 6/6/19
+function [status] = LoadYaoSequence(SEQ)
+    global gmSEQ;
+    
+    status = ClearAll();
+    status = status + SetState0(0);
+    % Given a SEQ structure, this function computes the delay times for all PB
+    % channels and sends the instructions to Cyclone 10 FPGA board
+    edID = 0;
+    % loop through all the channels and program each edge
+    period = 0;
+    for i=1:numel(SEQ.CHN)
+        chID = SEQ.CHN(i).PBN;
+        risefallidx = 0;
+        for j=1:SEQ.CHN(i).NRise
+            x = (SEQ.CHN(i).T(j) + SEQ.CHN(i).Delays(risefallidx + 1)) * 10^-9; %Delays is 2 element array... rise and fall?
+            dx = SEQ.CHN(i).DT(j) / SEQ.NSweepParam * 10^-9;
+%             steps = 
+            period = max(period, x + max(SEQ.NSweepParam * dx, 0));
+            %SetEdge(edID, enable, chID, x, dx)
+            status = status + SetEdge(edID, 1, chID, x, dx);
+            edID = edID + 1;
+            risefallidx = ~risefallidx;
+        end
+    end
+    pad = 500 * 10^-9;
+    status = status + SetPeriod(period + pad);
+    status = status + SetOuterPeriod(SEQ.Repeat);
+    % Add status = status + SetDPeriod(dperiod)?
 end
 
 function ohand = ObjHand()
@@ -60,6 +144,22 @@ function stat = SetPeriod(period)
     for i = 1:C10Const('write_attempts')
         fwrite(C10, C10Const('period'), 'uint8')
         fwrite(C10, int32(sec2clk(period)), 'int32');
+        fwrite(C10, C10Const('CR'));
+        fwrite(C10, C10Const('NL'));
+        ping = fread(C10, 1);
+        if ping == C10Const('period')
+            stat = 0;
+            return
+        end
+    end
+    stat = -1;
+end
+
+function stat = SetDPeriod(dperiod)
+    C10 = ObjHand();
+    for i = 1:C10Const('write_attempts')
+        fwrite(C10, C10Const('dperiod'), 'uint8')
+        fwrite(C10, int32(sec2clk(dperiod)), 'int32');
         fwrite(C10, C10Const('CR'));
         fwrite(C10, C10Const('NL'));
         ping = fread(C10, 1);
@@ -155,8 +255,50 @@ end
 
 %% Sequences
 
+
 %%Simple Rabi seq
-function stat = RabiConfigC10(steps, dt, counttime, pumptime, cushion)
+function stat = CWESR(steps, counttime, pumptime)
+    % Note that counttime must be >= 5us
+    % trig in own channel
+    % Chan 0 = laser
+    % Chan 1 = MW switch
+    % Chan 2 = Gates for counters 1 & 2 (signal and reference)
+
+    %% Rabi Parameters
+    counterpw = 30 * 10^-9; %30 ns pulse time for counter edges
+    trigwait = 5*10^-6;
+    minperiod = trigwait + 2 * counttime + pumptime + counterpw;
+    
+    %% Set up FPGA
+    stat = Cyc10FunctionPool('ClearAll');
+    stat = stat + Cyc10FunctionPool('SetPeriod', minperiod);
+    stat = stat + Cyc10FunctionPool('SetOuterPeriod', steps);
+
+% Note, DAQ counter uses only the rising edges, so 2 rising edges for start
+% and stop must be used
+%     Cyc10FunctionPool('SetEdge', edID, enable, chID, x, dx)
+
+    state0 = uint8(3); %0000_0000 start laser and MW on
+    stat = stat + Cyc10FunctionPool('SetState0', state0);
+    edges = {{1, 1, trigwait + counttime + counterpw, 0}, ... %Turn MW off
+             {1, 2, trigwait, 0},...  %Turn on data counter
+             {1, 2, trigwait + counterpw, 0},...%Falling edge for ^^
+             {1, 2, trigwait + counttime, 0},...  %Turn off data counter
+             {1, 2, trigwait + counttime + counterpw, 0},... % Falling edge for ^^
+             {1, 2, trigwait + counttime + pumptime, 0},...  %Turn on ref counter
+             {1, 2, trigwait + counttime + pumptime + counterpw, 0},... % Falling edge for ^^
+             {1, 2, trigwait + 2 * counttime + pumptime, 0},...  %Turn off ref counter
+             {1, 2, trigwait + 2 * counttime + pumptime + counterpw, 0} % Falling edge for ^^
+             };
+   
+    for i = 1:(length(edges))
+        ed = edges{i};
+        stat = stat + Cyc10FunctionPool('SetEdge', i-1,ed{1}, ed{2}, ed{3}, ed{4});
+    end
+end    
+
+%%Simple Rabi seq
+function stat = DarkVaryConfigC10(steps, dt, counttime, pumptime, cushion, AOMdelay)
     % Note that pumptime must be greater than counttime
     % Chan 0 = trig
     % Chan 1 = laser
@@ -164,33 +306,90 @@ function stat = RabiConfigC10(steps, dt, counttime, pumptime, cushion)
     % Chan 3 = Gates for counters 1 & 2 (signal and reference)
 
     %% Rabi Parameters
-    state0 = uint8(0); %0000_0000 start all off
-    counterpw = 30 * 10^-9; %30 ns pulse time for counter edges 
-%     steps = 100; %Data points to take for Rabi cycle
-    
+    counterpw = 30 * 10^-9; %30 ns pulse time for counter edges
+    AOMrisefall = 500*10^-9; %Measured as 400, 500 just to be safe
+%     trigwait = 0.75*10^-3;
+    trigwait = 2*10^-6; %Testing HW trigger
+    minperiod = trigwait + cushion + steps * dt + pumptime + AOMrisefall...
+        + counttime + counterpw + cushion * 2 + AOMdelay; % This is really slow, but it has to be for slowTrig
+
     %% Set up FPGA
     stat = Cyc10FunctionPool('ClearAll');
-    stat = stat + Cyc10FunctionPool('SetPeriod', pumptime + steps * dt...
-            + counttime + cushion * 20);
+    stat = stat + Cyc10FunctionPool('SetPeriod', minperiod);
     stat = stat + Cyc10FunctionPool('SetOuterPeriod', steps);
+
+    state0 = uint8(1); %0000_0000 start laser on
     stat = stat + Cyc10FunctionPool('SetState0', state0);
+    edges = {{1, 0, trigwait, 0}, ... % Turn Laser off
+             {1, 1, trigwait + cushion / 2, 0}, ... %Turn MW on
+             {1, 1, trigwait + cushion / 2, dt}, ... %Turn MW off, grows by dt each time
+             {1, 0, trigwait + cushion + steps * dt, 0}, ... % Turn Laser on
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay, 0},...  %Turn on data counter
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + counterpw, 0},...%Falling edge for ^^
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + counttime, 0},...  %Turn off data counter
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + counttime + counterpw, 0},... % Falling edge for ^^
+             {1, 0, trigwait + cushion + steps * dt + pumptime, 0}, ... % Turn Laser off
+             {1, 0, trigwait + cushion + steps * dt + pumptime + AOMrisefall, 0}, ... % Turn Laser back on             
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + pumptime + AOMrisefall, 0},...  %Turn on ref counter
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + pumptime + AOMrisefall + counterpw, 0},... % Falling edge for ^^
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + pumptime + AOMrisefall + counttime, 0},...  %Turn off ref counter
+             {1, 2, trigwait + cushion + steps * dt + AOMdelay + pumptime + AOMrisefall + counttime + counterpw, 0} % Falling edge for ^^
+             };
+    
+    for i = 1:(length(edges))
+        ed = edges{i};
+        stat = stat + Cyc10FunctionPool('SetEdge', i-1,ed{1}, ed{2}, ed{3}, ed{4}); %i-1+badedcount
+    end
+end    
+
+
+
+%%Simple Rabi seq
+function stat = RabiConfigC10(steps, dt, counttime, pumptime, AOMrisefall, AOMdelay, MWdelay)
+    % Note that pumptime must be greater than counttime
+    % Chan 0 = trig
+    % Chan 1 = laser
+    % Chan 2 = MW
+    % Chan 3 = Gates for counters 1 & 2 (signal and reference)
+
+    %% Rabi Parameters
+    counterpw = 30 * 10^-9; %30 ns pulse time for counter edges
+%     AOMrisefall = 800*10^-9; %Measured as 400, 800 just to be safe
+%     trigwait = 0.75*10^-3;
+%     MWdelay = 100 *10^-9;
+    pad = 1 * 10^-6;
+    trigwait = 2*10^-6; %Testing HW trigger
+    minperiod = trigwait + AOMrisefall + steps * dt + pumptime + AOMrisefall...
+        + counttime + counterpw + pad;
+
+    %% Set up FPGA
+    stat = Cyc10FunctionPool('ClearAll');
+    stat = stat + Cyc10FunctionPool('SetPeriod', minperiod);
+    stat = stat + Cyc10FunctionPool('SetOuterPeriod', steps);
 
 % Note, DAQ counter uses only the rising edges, so 2 rising edges for start
 % and stop must be used
-    % Cyc10FunctionPool('SetEdge', edID, enable, chID, x, dx)
-    % Should do this from file later
-    edges = {{1, 1, cushion, 0}, ... %Turn MW on
-             {1, 1, cushion, dt}, ... %Turn MW off, grows by dt each time
-             {1, 0, cushion + steps * dt, 0}, ... % Turn Laser on
-             {1, 2, cushion + steps * dt + cushion, 0},...  %Turn on data counter
-             {1, 2, cushion + steps * dt + cushion + counterpw, 0},...%Falling edge for ^^
-             {1, 2, cushion + steps * dt + cushion + counttime, 0},...  %Turn off data counter
-             {1, 2, cushion + steps * dt + cushion + counttime + counterpw, 0},... % Falling edge for ^^
-             {1, 2, cushion + steps * dt + cushion + pumptime - counttime, 0},...  %Turn on ref counter
-             {1, 2, cushion + steps * dt + cushion + pumptime - counttime + counterpw, 0},... % Falling edge for ^^
-             {1, 2, cushion + steps * dt + cushion + pumptime, 0},...  %Turn off ref counter
-             {1, 2, cushion + steps * dt + cushion + pumptime + counterpw, 0} % Falling edge for ^^
+%     Cyc10FunctionPool('SetEdge', edID, enable, chID, x, dx)
+%     Should do this from file later
+
+    state0 = uint8(1); %0000_0000 start laser on
+    stat = stat + Cyc10FunctionPool('SetState0', state0);
+    edges = {{1, 0, trigwait, 0}, ... % Turn Laser off
+             {1, 1, trigwait + AOMrisefall, 0}, ... %Turn MW on
+             {1, 1, trigwait + AOMrisefall, dt}, ... %Turn MW off, grows by dt each time
+             {1, 0, trigwait + AOMrisefall + steps * dt + MWdelay, 0}, ... % Turn Laser on
+             {1, 2, trigwait + AOMrisefall + steps * dt + MWdelay + AOMdelay, 0},...  %Turn on data counter
+             {1, 2, trigwait + AOMrisefall + steps * dt + MWdelay + AOMdelay + counterpw, 0},...%Falling edge for ^^
+             {1, 2, trigwait + AOMrisefall + steps * dt + MWdelay + AOMdelay + counttime, 0},...  %Turn off data counter
+             {1, 2, trigwait + AOMrisefall + steps * dt + MWdelay + AOMdelay + counttime + counterpw, 0},... % Falling edge for ^^
+             {1, 0, trigwait + AOMrisefall + steps * dt + pumptime, 0}, ... % Turn Laser off
+             {1, 0, trigwait + AOMrisefall + steps * dt + pumptime + AOMrisefall, 0}, ... % Turn Laser back on             
+             {1, 2, trigwait + AOMrisefall + steps * dt + AOMdelay + pumptime + AOMrisefall, 0},...  %Turn on ref counter
+             {1, 2, trigwait + AOMrisefall + steps * dt + AOMdelay + pumptime + AOMrisefall + counterpw, 0},... % Falling edge for ^^
+             {1, 2, trigwait + AOMrisefall + steps * dt + AOMdelay + pumptime + AOMrisefall + counttime, 0},...  %Turn off ref counter
+             {1, 2, trigwait + AOMrisefall + steps * dt + AOMdelay + pumptime + AOMrisefall + counttime + counterpw, 0} % Falling edge for ^^
              };
+    
     for i = 1:(length(edges))
         ed = edges{i};
         stat = stat + Cyc10FunctionPool('SetEdge', i-1,ed{1}, ed{2}, ed{3}, ed{4}); %i-1+badedcount
@@ -220,6 +419,8 @@ function const = C10Const(what)
             const = uint8(6);
         case 'soft_trig'
             const = uint8(7);
+        case 'dperiod'
+            const = uint8(8); %Not functional yet            
         case 'CR'
             const = 13;
         case 'NL'
